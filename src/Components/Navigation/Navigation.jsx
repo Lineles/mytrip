5b7fe042a4b1bac94c2a29d269ef1aa9a84833ef
Lineles <106559385+Lineles@users.noreply.github.com>
2022-10-04 14:22:52 +0200

    created basic nav-bar

--- src/Components/Navigation/Navigation.jsx
+++ src/Components/Navigation/Navigation.jsx
@@ -1,3 +1,5 @@
+import { Link } from "react-router-dom";
+
 import "./Navigation.css"
 
 
@@ -6,9 +8,12 @@ function Navigation ( ) {
 
     return( 
 
-        <div> 
-
-
+        <div className="Navigation-Container"> 
+                <Link to="/" className="nav-link">Home Page</Link>
+                <Link to="/News" className="nav-link">News</Link>
+                <Link to="/Countries" className="nav-link">Countries</Link>
+                <Link to="/Shop" className="nav-link">Shop</Link>
+                <Link to="/Registration" className="nav-link">Create Acc</Link>
         </div>
     )
 } 
