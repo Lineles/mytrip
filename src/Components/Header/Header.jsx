de27c7246da38dd1a10126013f81fe29c0bbda06
Lineles <106559385+Lineles@users.noreply.github.com>
2022-10-04 14:03:05 +0200

    header

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -5,8 +5,10 @@ function Header ( ) {
 
     return( 
 
-        <div> 
-            
+        <div className="header-container"> 
+            <img src="\IMAGES\GOPR4922-banner4.JPG" alt="" className="header-banner"></img>
+            <h1 className="header-h1"> My Trip </h1>
+          
 
         </div>
     )
