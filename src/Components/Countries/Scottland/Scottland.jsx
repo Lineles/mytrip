f21c85cff268acc0e06fec5369f00da66d8207af
Lineles <106559385+Lineles@users.noreply.github.com>
2022-10-05 12:08:53 +0200

    Top 10 to see in Scottland

--- src/Components/Countries/Scottland/Scottland.jsx
+++ src/Components/Countries/Scottland/Scottland.jsx
@@ -5,8 +5,34 @@ function Scottland ( ) {
 
     return( 
 
-        <div> 
-            
+        <div className="Scottland-main-container"> 
+            <div>
+                <h1> Top 10 to see </h1>
+                    <ol>
+                        <li><a href="https://en.wikipedia.org/wiki/Stirling_Castle" >Stirling Castle</a></li>
+                        <li><a href="https://en.wikipedia.org/wiki/Kelvingrove_Art_Gallery_and_Museum" >Kelvingrove Art Gallery and Museum</a></li>
+                        <li><a href="https://en.wikipedia.org/wiki/Ben_Nevis" >Ben Nevis</a></li>
+                        <li><a href="https://en.wikipedia.org/wiki/National_Museum_of_Scotland" >National Museum of Scotland</a></li>
+                        <li><a href="https://www.glasgowlife.org.uk/museums/venues/riverside-museum" >Riverside Museum</a></li>
+                        <li><a href="https://en.wikipedia.org/wiki/HMY_Britannia" >The Royal Yacht Britannia</a></li>
+                        <li><a href="https://en.wikipedia.org/wiki/Edinburgh_Castle" >Edinburgh Castle</a></li>
+                        <li><a href="https://en.wikipedia.org/wiki/Old_Town,_Edinburgh" >Edinburgh Old Town</a></li>
+                        <li><a href="https://en.wikipedia.org/wiki/Loch_Ness" >Loch Ness</a></li>
+                        <li><a href="https://en.wikipedia.org/wiki/Arthur%27s_Seat" >Arthur's Seat</a></li>
+                    </ol>
+
+
+
+            </div>
+
+
+
+
+
+
+
+
+
 
         </div>
     )
