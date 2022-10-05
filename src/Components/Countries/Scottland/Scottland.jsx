23353c79cbfa3b3d95f2a64807f083bccd5a04ec
Lineles <106559385+Lineles@users.noreply.github.com>
2022-10-05 15:37:36 +0200

    ceratet north cost 500

--- src/Components/Countries/Scottland/Scottland.jsx
+++ src/Components/Countries/Scottland/Scottland.jsx
@@ -6,9 +6,9 @@ function Scottland ( ) {
     return( 
 
         <div className="Scottland-main-container"> 
-            <div>
+            <div className="Scottland-top10">
                 <h1> Top 10 to see </h1>
-                    <ol>
+                    <ul>
                         <li><a href="https://en.wikipedia.org/wiki/Stirling_Castle" >Stirling Castle</a></li>
                         <li><a href="https://en.wikipedia.org/wiki/Kelvingrove_Art_Gallery_and_Museum" >Kelvingrove Art Gallery and Museum</a></li>
                         <li><a href="https://en.wikipedia.org/wiki/Ben_Nevis" >Ben Nevis</a></li>
@@ -19,12 +19,30 @@ function Scottland ( ) {
                         <li><a href="https://en.wikipedia.org/wiki/Old_Town,_Edinburgh" >Edinburgh Old Town</a></li>
                         <li><a href="https://en.wikipedia.org/wiki/Loch_Ness" >Loch Ness</a></li>
                         <li><a href="https://en.wikipedia.org/wiki/Arthur%27s_Seat" >Arthur's Seat</a></li>
-                    </ol>
+                    </ul>
+            </div>
+            <div className="Scottland-roadtrips">
+                <h1> North Cost 500 </h1>
+                <div className="Scottland-roadtrips-flex">
+                    <div className="Scottland-roadtrips-info">
+                        <p>Length:	516 mi (830 km)</p>
+                        <p>Country:	United Kingdom</p>
+                        <p>Primary destinations;	Inverness, Lochcarron, Applecross, Gairloch, Ullapool, Lochinver, Durness, Thurso, John o' Groats, Wick, Helmsdale, Dornoch, Invergordon, Dingwall</p>
+                        <p>The North Coast 500 is a 516-mile (830 km) scenic route around the north coast of Scotland, starting and ending at Inverness Castle. The route is also known as the NC500 and was launched in 2015, linking many features in the north Highlands of Scotland in one touring route.
+                            The route has increased visitor numbers to parts of the north of Scotland, which has brought economic benefits to some areas. The increase in traffic has also led to more instances of bad and dangerous driving, as well as damage to the roads and concerns about the environmental impact.</p>
+                    </div>
 
+                    <img src="\IMAGES\Scottland\NorthCost500.PNG" alt="" className="Scottland-roadtrips-NorthCost500"></img>
+                </div>
+            </div>
+            <div className="Scottland-funnstuff">
 
 
             </div>
+            <div className="Scottland-coments">
 
+
+            </div>
 
 
 
